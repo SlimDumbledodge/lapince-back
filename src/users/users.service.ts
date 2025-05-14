18b4c665339f90d6ef639832cb83d7e12307e3d1
Mederick-ROBERT <mederick.robert@free.fr>
2025-05-14 16:28:12 +0200

    ✨ feat: update UsersService to handle user creation, retrieval, and updates with email validation and password hashing; refactor UsersController to remove unnecessary type conversions and comment out delete method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, BadRequestException, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DrizzleAsyncProvider } from 'src/db/drizzle/drizzle.provider';
+import * as schema from '../db/schema';
+import { eq, asc } from 'drizzle-orm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @Inject(DrizzleAsyncProvider) private db: NodePgDatabase<typeof schema>,
+  ) {}
+
+  /**
+   * Create a new user
+   * => This function will hash the password before saving it to the database
+   * @param createUserDto
+   * @returns schema.users.$inferInsert
+   */
+  async create(createUserDto: CreateUserDto): Promise<schema.User> {
+    // Verify if the email is unique in db
+    const emailResponse = await this.findByEmail(createUserDto.email)
+
+    if (emailResponse !== null || emailResponse !== undefined) {
+      throw new BadRequestException('This email is already set !')
+    }
+
+    // Hash the user password
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    
+    const newUser = await this.db.insert(schema.users).values({
+      firstName : createUserDto.firstName,
+      lastName : createUserDto.lastName,
+      email : createUserDto.email,
+      password: hashedPassword,
+      createdAt: new Date()
+    }).returning()
+
+    return newUser[0]
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(): Promise<schema.User[]> {
+    return this.db.select().from(schema.users).orderBy(asc(schema.users.lastName))
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string): Promise<schema.User> {
+    const result = await this.db.select().from(schema.users).where(eq(schema.users.id, id))
+
+    if (result.length === 0) {
+      throw new NotFoundException(`A user with this id ()`)
+    }
+
+    return result[0]
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findByEmail(email: string): Promise<schema.User|null> {
+    const result = await this.db.select().from(schema.users).where(eq(schema.users.email, email))
+
+    return result[0]
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<schema.User> {
+    if (updateUserDto.email) {
+      // Verify if the email is not already used
+      const emailResponse = await this.findByEmail(updateUserDto.email)
+
+      if (emailResponse !== null) {
+        if (emailResponse.id !== id) {
+          throw new BadRequestException('This email is already set !')
+        }
+      }
+    }
+
+    const result = await this.db.update(schema.users).set({
+      ...updateUserDto,
+      updatedAt: new Date()
+    })
+    .where(eq(schema.users.id, id))
+    .returning()
+
+    if (result.length === 0) {
+      throw new BadRequestException('User not found')
+    }
+
+    return result[0]
   }
+
+  async updatePassword() {
+    return 'This function update the user password'
+  }
+
+  // remove(id: string) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
