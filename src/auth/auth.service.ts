9509a2652bf28027df5d8bdca896a5832c77ee3d
Mederick-ROBERT <mederick.robert@free.fr>
2025-06-03 14:22:02 +0200

    ✨ feat : implement refresh token functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,9 @@ import { JwtService } from '@nestjs/jwt';
 import 'dotenv/config'
 import {RegisterDto} from "./dto/register.dto";
 import * as schema from '../db/schema';
+import { DrizzleAsyncProvider } from 'src/db/drizzle/drizzle.provider';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import ms from 'ms';
 
 @Injectable()
 export class AuthService {
@@ -13,6 +16,7 @@ export class AuthService {
     @Inject(UsersService) private readonly usersService: UsersService,
     @Inject(JwtService) private readonly jwtService: JwtService,
     @Inject(UserAccountService) private readonly userAccountService: UserAccountService,
+    @Inject(DrizzleAsyncProvider) private readonly db: NodePgDatabase<typeof schema>,
   ) {}
 
   async signUp(registerDto: RegisterDto) {
@@ -61,6 +65,8 @@ export class AuthService {
   private async createToken(user: schema.User & {accountName: string, amount: number}) {
     const payload = { email: user.email, sub: user.id };
 
+    const refresh_token = await this.createRefreshToken(user);
+
     return {
       user: {
         id: user.id,
@@ -70,7 +76,30 @@ export class AuthService {
         accountName: user.accountName,
         amount: user.amount,
       },
-      access_token: await this.jwtService.signAsync(payload, {expiresIn: process.env.JWT_EXPIRES_IN ?? '6h'}),
+      access_token: await this.jwtService.signAsync(payload, {expiresIn: process.env.JWT_EXPIRES_IN ?? '15m'}),
+      access_token_expires_at: new Date(Date.now() + ms(process.env.JWT_EXPIRES_IN ?? '15m')),
+      refresh_token: refresh_token.refresh_token,
+      refresh_token_expires_at: refresh_token.expiresAt,
+    };
+  }
+
+  private async createRefreshToken(user: schema.User) {
+    const payload = { sub: user.id };
+
+    const refresh_token = await this.jwtService.signAsync(payload, {expiresIn: process.env.JWT_REFRESH_EXPIRES_IN?? '7d'});
+    const expiresAt = new Date(Date.now() + ms(process.env.JWT_REFRESH_EXPIRES_IN?? '7d'));
+
+    // TODO : get IP address and User Agent in the request
+
+    await this.db.insert(schema.sessions).values({
+      userId: user.id,
+      tokenHash: await bcrypt.hash(refresh_token, 10),
+      expiresAt
+    })
+
+    return {
+      refresh_token,
+      expiresAt
     };
   }
 }
