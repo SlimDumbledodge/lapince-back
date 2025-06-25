5949ffd7c0ca5947f376997dd12473af48e7dd30
Mederick-ROBERT <mederick.robert@free.fr>
2025-06-25 16:24:23 +0200

    🔧 fix(transactions): add ParseBoolPipe to updateNextChilds query parameter in update transaction method.

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ParseUUIDPipe, Query } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseBoolPipe, ParseUUIDPipe, Query } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto, CreateTransactionSchema } from './dto/create-transaction.dto';
 import { UpdateTransactionDto, UpdateTransactionSchema } from './dto/update-transaction.dto';
@@ -57,7 +57,7 @@ export class TransactionsController {
     @Param('id', ParseUUIDPipe) id: string, 
     @Body(new ZodValidationPipe(UpdateTransactionSchema)) updateTransactionDto: UpdateTransactionDto,
     @User() user: UserEntity,
-    @Query('updateNextChilds') updateNextChilds: boolean = false,
+    @Query('updateNextChilds', ParseBoolPipe) updateNextChilds: boolean = false,
   ) {
     return this.transactionsService.update(id, updateTransactionDto, user.id, updateNextChilds);
   }
